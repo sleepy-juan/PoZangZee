e002f43d1933740b5695ee6c10dfda0f879c2a9d
Juan Lee <juanlee@kaist.ac.kr>
2019-05-07 17:43:38 +0900

    Add Documents (ing)

---
+++ src/pages/Docs-Kr.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderHTML from 'react-render-html';
+
+class App extends React.Component {
+    state = {
+        html: "<p>Loading...</p>"
+    }
+    componentDidMount(){
+        fetch("https://raw.githubusercontent.com/sleepy-juan/PoZangZee/master/documents/docs-kr.html")
+        .then(html => html.text())
+        .then(html => {
+            this.setState({
+                html
+            })
+        })
+    }
+    render() {
+      return (
+        <div className='app'>
+            {renderHTML(this.state.html)}
+        </div>
+    );
+    }
+  }
+
+export default App;
