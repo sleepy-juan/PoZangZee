dcd0f06848a4c1e23c095c963db3793814b36e42
Changhyeon <pch313@kaist.ac.kr>
2019-05-12 14:42:02 +0900

    insert format saving page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Home from './pages/Home';
+import Format from './pages/Format';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Format />
     </div>
   );
 }
